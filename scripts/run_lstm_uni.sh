090f79d7aa14f78d354151b9003a159b68371337
Sukrit Rao <str8775@nyu.edu>
2023-09-11 22:12:45 -0400

    working scripts for role exps with attention

--- scripts/run_lstm_uni.sh
+++ scripts/run_lstm_uni.sh
@@ -2,46 +2,56 @@
 
 export DATA_PATH=$1                            # OpenNMT-preprocessed data path
 export OPENNMT_PATH=$2                         # OpenNMT path
+export SEED=$3                                  # Random seed
 export EXAMPLES=1_example                      # Number of exposure examples (1 or 100)
-export SAVE_PATH=${OPENNMT_PATH}/tf_checkpoints  # Save path for checkpoints
-export SAVE_NAME=${EXAMPLES}_lstm_uni_no_att_1layers_no_if_proj          # Checkpoint name
-export LOG_PATH=${OPENNMT_PATH}/logs           # Log path
-export PRED_PATH=${OPENNMT_PATH}/preds         # Predictions path
-export SEED=42                                  # Random seed
-export CUDA_VISIBLE_DEVICES=-1                  # GPU machine number
-export LOAD_CHECKPOINT=/scratch/str8775/conda_env/compositional-generalisation/COGS/src/OpenNMT-py/tf_checkpoints/1_example_gru_uni_no_att_1layers_no_if/s42_best.pt                      # load saved checkpoint
+export SAVE_PATH=${OPENNMT_PATH}/tf_checkpoints/1_layer_with_attn_new_vocab_debugging/lstm  # Save path for checkpoints
+export SAVE_NAME=seed_${SEED}_${EXAMPLES}_lstm_uni_att_1layers_no_if_hidden512          # Checkpoint name
+# export SAVE_NAME=${EXAMPLES}_gru_uni_no_att_1layers_no_if_hidden512
+export LOG_PATH=${OPENNMT_PATH}/logs/1_layer_with_attn_new_vocab_debugging/lstm/lstm_seed_${SEED}          # Log path
+export PRED_PATH=${OPENNMT_PATH}/preds/1_layer_with_attn_new_vocab_debugging/lstm/lstm_seed_${SEED}         # Predictions path
+export CUDA_VISIBLE_DEVICES=0                # GPU machine number
+# export LOAD_CHECKPOINT=/scratch/str8775/conda_env/compositional-generalisation/COGS/src/OpenNMT-py/tf_checkpoints/1_example_gru_uni_att_1layers_no_if_hidden512/s42_step_65000.pt # load saved checkpoint
 
-mkdir $LOG_PATH
-mkdir $PRED_PATH
+mkdir -p $LOG_PATH
+mkdir -p $PRED_PATH
 
-# Training
-python -u $OPENNMT_PATH/train.py -config ./config/build_vocab_1_example.yml -save_model $SAVE_PATH/${SAVE_NAME}/s$SEED \
-	-src_vocab $DATA_PATH/$EXAMPLES/${EXAMPLES}.vocab.src \
-	-tgt_vocab $DATA_PATH/$EXAMPLES/${EXAMPLES}.vocab.tgt \
-	-layers 1 -rnn_size 512 -word_vec_size 512 \
-	-encoder_type rnn -decoder_type rnn -rnn_type LSTM \
-	-input_feed 0 -global_attention none \
-	-train_steps 30000  -max_generator_batches 2 -dropout 0.0 \
-	-batch_size 256 -batch_type sents -normalization sents  -accum_count 4 \
-	-optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
-       -max_grad_norm 5 -param_init 0  \
-	-valid_steps 500 -save_checkpoint_steps 500 \
-	-early_stopping 15 --early_stopping_criteria accuracy \
-	-world_size 1 -gpu_ranks 0 -seed $SEED --log_file ${LOG_PATH}/${SAVE_NAME}_s${SEED}.log \
-	# -train_from ${LOAD_CHECKPOINT}
+# -config ./config/build_vocab_1_example.yml \
+	# -src_vocab $DATA_PATH/$EXAMPLES/${EXAMPLES}.vocab.src \
+	# -tgt_vocab $DATA_PATH/$EXAMPLES/${EXAMPLES}.vocab.tgt \
+
+# # Training
+# python -u $OPENNMT_PATH/train.py \
+#  	-save_model $SAVE_PATH/${SAVE_NAME}/s$SEED \
+#  	-src_vocab $DATA_PATH/new_vocab_exp_attn_role/$EXAMPLES/source_vocab.txt \
+#  	-tgt_vocab $DATA_PATH/new_vocab_exp_attn_role/$EXAMPLES/target_vocab.txt \
+#  	-data $DATA_PATH/new_vocab_exp_attn_role/$EXAMPLES/data.yml \
+#  	-layers 1 -rnn_size 512 -word_vec_size 512 \
+#  	-encoder_type rnn -decoder_type rnn -rnn_type LSTM \
+#  	-input_feed 0 -global_attention dot \
+# 	-train_steps 80000  -max_generator_batches 2 -dropout 0.1 \
+#  	-batch_size 512 -batch_type sents -normalization sents  -accum_count 4 \
+#         -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
+#         -max_grad_norm 5 -param_init 0  \
+#  	-valid_steps 1000 -save_checkpoint_steps 1000 \
+#  	-early_stopping 10 --early_stopping_criteria accuracy \
+#  	-world_size 1 -gpu_ranks 0 -seed $SEED --log_file ${LOG_PATH}/${SAVE_NAME}_s${SEED}.log
+# # # # # 	# -train_from ${LOAD_CHECKPOINT}
 
 
 
 # Inference
-# for SPLIT in gen test dev
-# for SPLIT in dev
-# do
-#     python $OPENNMT_PATH/translate.py -model $SAVE_PATH/${SAVE_NAME}/s${SEED}_best.pt \
-#                                       -src $DATA_PATH/${SPLIT}_source.txt \
-#                                       -tgt $DATA_PATH/${SPLIT}_target.txt \
-#                                       -output ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}.txt \
-#                                       -verbose -shard_size 0 \
-#                                       -gpu 0 -batch_size 128  > ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}_log.txt
+#for SPLIT in gen
+# # # #
+for SPLIT in gen test train dev
+# # # for SPLIT in dev
 
-#     paste $DATA_PATH/${SPLIT}_source.txt $DATA_PATH/${SPLIT}_target.txt ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}.txt > ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}.tsv
-# done
+do
+   python $OPENNMT_PATH/translate.py -model $SAVE_PATH/${SAVE_NAME}/s${SEED}_best.pt \
+                                     -src $DATA_PATH/new_vocab_exp_attn_role/$EXAMPLES/${SPLIT}_source.txt \
+                                     -tgt $DATA_PATH/new_vocab_exp_attn_role/$EXAMPLES/${SPLIT}_target.txt \
+                                     -output ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}.txt \
+                                     -verbose -shard_size 0 \
+                                     -gpu ${CUDA_VISIBLE_DEVICES} -batch_size 512  > "${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}_log.txt"
+
+   paste $DATA_PATH/${SPLIT}_source.txt $DATA_PATH/${SPLIT}_target.txt ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}.txt > ${PRED_PATH}/${SPLIT}_pred_${SAVE_NAME}_s${SEED}.tsv
+done
--- scripts/run_prepare_role_data.sh
+++ scripts/run_prepare_role_data.sh
@@ -4,14 +4,14 @@ export DATA_PATH=$1                            # OpenNMT-preprocessed data path
 export OPENNMT_PATH=$2                         # OpenNMT path
 export SEED=$3                                  # Random seed
 export EXAMPLES=1_example                      # Number of exposure examples (1 or 100)
-export SAVE_PATH=${OPENNMT_PATH}/tf_checkpoints/1_layer_with_attn_new_vocab_debugging  # Save path for checkpoints
-export SAVE_NAME=seed_${SEED}_${EXAMPLES}_gru_uni_att_1layers_no_if_hidden512          # Checkpoint name
+export SAVE_PATH=${OPENNMT_PATH}/tf_checkpoints/1_layer_with_attn_new_vocab_debugging/lstm  # Save path for checkpoints
+export SAVE_NAME=seed_${SEED}_${EXAMPLES}_lstm_uni_att_1layers_no_if_hidden512          # Checkpoint name
 # export SAVE_NAME=${EXAMPLES}_gru_uni_no_att_1layers_no_if_hidden512
-export LOG_PATH=${OPENNMT_PATH}/logs/1_layer_with_attn_new_vocab_debugging/seed_${SEED}          # Log path
-export PRED_PATH=${OPENNMT_PATH}/preds/1_layer_with_attn_new_vocab_debugging/seed_${SEED}         # Predictions path
-export CUDA_VISIBLE_DEVICES=-1                # GPU machine number
-export MODEL_SAVE_DIR=../role_data/1_layer_with_attn_new_vocab/cogs_${SAVE_NAME}/saved_models # Location of exported cogs decoder weights
-export EMBD_OUTPUT_SAVE_NAME=../role_data/1_layer_with_attn_new_vocab/cogs_${SAVE_NAME}/cogs_${SAVE_NAME}_embd
+export LOG_PATH=${OPENNMT_PATH}/logs/1_layer_with_attn_new_vocab_debugging/lstm/seed_${SEED}          # Log path
+export PRED_PATH=${OPENNMT_PATH}/preds/1_layer_with_attn_new_vocab_debugging/lstm/seed_${SEED}         # Predictions path
+export CUDA_VISIBLE_DEVICES=0                # GPU machine number
+export MODEL_SAVE_DIR=../role_data/1_layer_with_attn_new_vocab_debugging/lstm/cogs_${SAVE_NAME}/saved_models # Location of exported cogs decoder weights
+export EMBD_OUTPUT_SAVE_NAME=../role_data/1_layer_with_attn_new_vocab_debugging/lstm/cogs_${SAVE_NAME}/cogs_${SAVE_NAME}_embd
 
 mkdir $LOG_PATH
 mkdir $PRED_PATH
@@ -19,7 +19,7 @@ mkdir -p ${MODEL_SAVE_DIR}
 
 # Inference
 # for SPLIT in dev
-for SPLIT in gen dev test
+for SPLIT in gen dev test train
 do
     python $OPENNMT_PATH/prepare_role_data.py -model $SAVE_PATH/${SAVE_NAME}/s${SEED}_best.pt \
                                               -src $DATA_PATH/new_vocab_exp_attn_role/$EXAMPLES/${SPLIT}_source.txt \
@@ -29,7 +29,7 @@ do
                                               -model_prefix ${SAVE_NAME} \
                                               -verbose -shard_size 0 \
                                               -gpu ${CUDA_VISIBLE_DEVICES} -batch_size 1 \
-                                              -rnn_type GRU \
+                                              -rnn_type LSTM \
                                               -layers 1 -attn_model  \
                                               -data_split ${SPLIT}
                                                #-debug
